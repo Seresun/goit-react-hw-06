f4cf64233eb16cdd69eece0ee82d37bab17aad81
Seresun <slavnyunstoppable22@gmail.com>
2024-10-28 20:38:39 +0100

    fixv4

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,15 @@
-import { useSelector, useDispatch } from "react-redux";
-import { deleteContact, selectContacts } from "../../redux/contactsSlice";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import Contact from "../Contact/Contact";
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
-  const dispatch = useDispatch();
 
   return (
     <ul className={styles.list}>
       {contacts.map((contact) => (
-        <li key={contact.id}>
-          {contact.name}: {contact.number}
-          <button onClick={() => dispatch(deleteContact(contact.id))}>
-            Delete
-          </button>
-        </li>
+        <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
   );
